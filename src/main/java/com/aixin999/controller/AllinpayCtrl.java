6374253b6058ac589a3dfb68a360db203b612ade
vevoly <jevoly@163.com>
2017-09-05 21:20:39 +0800

    解决json乱码问题

--- src/main/java/com/aixin999/controller/AllinpayCtrl.java
+++ src/main/java/com/aixin999/controller/AllinpayCtrl.java
@@ -29,16 +29,39 @@ public class AllinpayCtrl {
     String format = PropertyUtil.getValueByKey(propFile, "format");
     String httpUrl = PropertyUtil.getValueByKey(propFile, "http_url");*/
 
+    /**
+     * app key
+     */
     @Value("#{cfg.app_key}")
     String appKey;
+
+    /**
+     * 数据密钥，用户密码加密
+     */
     @Value("#{cfg.secret_key}")
     String secretKey;
+
+    /**
+     * api版本号
+     */
     @Value("#{cfg.v}")
     String v;
+
+    /**
+     * 签名版本号
+     */
     @Value("#{cfg.sign_v}")
     String signV;
+
+    /**
+     * 返回数据格式
+     */
     @Value("#{cfg.format}")
     String format;
+
+    /**
+     * 调用接口地址
+     */
     @Value("#{cfg.http_url}")
     String httpUrl;
 
@@ -48,13 +71,16 @@ public class AllinpayCtrl {
      * @param password 密码
      * @return 卡信息
      */
-    @RequestMapping(value = "getCardInfo", method = RequestMethod.GET)
+    @RequestMapping(value = "getCardInfo", method = RequestMethod.GET, produces = "text/json;charset=UTF-8")
     @ResponseBody
     public String getCardInfo(String cardId, String password) {
 
+        //调用接口方法名
         String method = PropertyUtil.getValueByKey("cfg.properties", "method.get_card_info");
+        //当前时间戳
         String timestamp = DateTools.dateToNum14(new Date());
         String ret = null;
+        //调用接口参数
         Map<String, Object> params = new HashMap<String, Object>();
         params.put("timestamp", timestamp);
         params.put("method", method);
@@ -62,11 +88,14 @@ public class AllinpayCtrl {
         params.put("v", v);
         params.put("app_key", appKey);
         params.put("card_id", cardId);
+        //获取加密后的密码
         params.put("password", AllinpayUtil.passwordCrypto(password, secretKey));
         params.put("sign_v", signV);
 
         try {
+            //组装参数
             String paramString = AllinpayUtil.buildParams(params);
+            //调用接口并获取返回数据
             ret = HttpClientUtil.sendGet(httpUrl, paramString);
         } catch (Exception e) {
             e.printStackTrace();
@@ -74,7 +103,7 @@ public class AllinpayCtrl {
         return ret;
     }
 
-    @RequestMapping(value = "getCardLog", method = RequestMethod.GET)
+    @RequestMapping(value = "getCardLog", method = RequestMethod.GET, produces = "text/json;charset=UTF-8")
     @ResponseBody
     public String getCardLog(String cardId, String password, String beginDate, String endDate, String pageNo, String pageSize) {
 
